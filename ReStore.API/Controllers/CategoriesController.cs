d6f89d21d902a0aa10e5fb19ab2ac80b92dd479c
Youssef Hamdy <joehamdy288@gmail.com>
2026-04-28 01:30:57 +0200

    Refactor CategoriesController: Fix object cycle and implement CategoryDto

--- ReStore.API/Controllers/CategoriesController.cs
+++ ReStore.API/Controllers/CategoriesController.cs
@@ -18,38 +18,49 @@ namespace ReStore.API.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<CategoryDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetCategories()
         {
-            var categories = await _context.Categories.ToListAsync();
+            // بنجيب الداتا ونحولها لـ DTO عشان السواجر والفرونت إند
+            var categories = await _context.Categories
+                .Select(c => new CategoryDto
+                {
+                    Id = c.Id,
+                    Name = c.Name
+                })
+                .ToListAsync();
             
             return Ok(categories);
         }
 
         [HttpPost]
-        [HttpPost]
-public async Task<ActionResult<CategoryDto>> CreateCategory(CategoryCreateDto categoryDto)
-{
-    var category = new Category
-    {
-        Name = categoryDto.Name,
-        
-    };
+        [ProducesResponseType(typeof(CategoryDto), StatusCodes.Status200OK)]
+        public async Task<ActionResult<CategoryDto>> CreateCategory(CategoryCreateDto categoryDto)
+        {
+            var category = new Category
+            {
+                Name = categoryDto.Name,
+            };
 
-    _context.Categories.Add(category);
-    await _context.SaveChangesAsync();
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
 
-    var resultDto = new CategoryDto
-    {
-        Id = category.Id,
-        Name = category.Name,
-        
-    };
+            var resultDto = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+            };
 
-    return Ok(resultDto);
-}
+            return Ok(resultDto);
+        }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateCategory(int id, [FromBody] Category updatedCategory)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        // استخدمنا DTO هنا بدل Entity عشان نحمي الكود
+        public async Task<IActionResult> UpdateCategory(int id, [FromBody] CategoryDto updatedCategory)
         {
             var category = await _context.Categories.FindAsync(id);
 
@@ -63,10 +74,19 @@ public async Task<ActionResult<CategoryDto>> CreateCategory(CategoryCreateDto ca
 
             await _context.SaveChangesAsync();
 
-            return Ok(new { message = "Category updated successfully.", category });
+            // حولنا النتيجة لـ DTO عشان نتجنب إيرور الـ Object Cycle
+            var resultDto = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name
+            };
+
+            return Ok(new { message = "Category updated successfully.", category = resultDto });
         }
 
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> DeleteCategory(int id)
         {
             var category = await _context.Categories.FindAsync(id);
